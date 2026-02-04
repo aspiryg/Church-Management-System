8d6b9754534ea2f847dc5638d370f0b153192a17
Ahmad Spierij <ahmadspierij@gmail.com>
2026-02-03 23:49:25 -0500

    Add project files.

---
+++ ChurchMS_Backend/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using ChurchMS_Backend.Entities;
+using ChurchMS_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ChurchMS_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IConfiguration configuration) : ControllerBase
+    {
+        public static User user = new User();
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+            if (new PasswordHasher<User>()
+                .VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password.");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            //
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+            return jwt;
+        }
+    }
+}
---
+++ ChurchMS_Backend/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ChurchMS_Backend.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
